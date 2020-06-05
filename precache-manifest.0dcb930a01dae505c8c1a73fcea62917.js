c971d7b0acfb737969ffe4635e4a3478eb8131a3
Luis Preciado <luisapreciado99@gmail.com>
2020-06-05 14:21:09 -0500

    Updates

---
+++ precache-manifest.0dcb930a01dae505c8c1a73fcea62917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a08608904c7ba443a00711e7ae44d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1c016b61af83c21806",
+    "url": "/static/css/main.6eb8b2d6.chunk.css"
+  },
+  {
+    "revision": "b77e16e100eef26ef352",
+    "url": "/static/js/2.575133b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.575133b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1c016b61af83c21806",
+    "url": "/static/js/main.cca7c0bf.chunk.js"
+  },
+  {
+    "revision": "5a8ed2dc9a5b0aa94c1e",
+    "url": "/static/js/runtime-main.45c90165.js"
+  }
+]);
